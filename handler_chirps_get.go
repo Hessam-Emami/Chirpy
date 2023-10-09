ad0bb6845f418526f1b40baa12609deaf33d35ad
Hessam Emami <hessam.nodeh@goflink.com>
2023-10-09 10:57:49 +0200

    Add get chirp by id endpoint

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"sort"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
@@ -25,4 +28,33 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	})
 
 	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	idString := chi.URLParam(r, "chirpID")
+	if len(idString) == 0 {
+		respondWithError(w, http.StatusBadRequest, "chirp id is not provided")
+		return
+	}
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "chirp id must be an integer")
+		return
+	}
+	dbChirps, err := cfg.DB.GetChirps()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
+		return
+	}
+
+	chirp := Chirp{}
+	for _, dbChirp := range dbChirps {
+		if dbChirp.ID == id {
+			chirp = Chirp(dbChirp)
+			respondWithJSON(w, http.StatusOK, chirp)
+			return
+		}
+	}
+
+	respondWithError(w, http.StatusNotFound, "Chirp not found")
 }
